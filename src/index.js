80566f9636545329ae029da3eaa95d0fa4fbc9a8
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-18 13:55:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   
 let button = document.querySelector("form");
 button.addEventListener("submit", append);
-let ul=document.getElementById()
+let ul=document.getElementById("tasks");
 function append(e) {
   let cal = document.getElementById("new-task-description").value;
   let li = document.createElement("li");
   let dis = cal;
   li.innerHTML = dis;
   ul.appendChild(li);
-  let resetForm = document.getElementById("form1");
-  resetForm.reset();
   e.preventDefault();
 }
   
